b5f7eb914d272e7e63b9e77fddf4c94db962d99c
Jesica Formoso <Jesica.formoso@gmail.com>
2026-04-13 10:23:14 -0300

    Fix corregir nombre de dataframe

--- R/cursos-md.R
+++ R/cursos-md.R
@@ -15,7 +15,7 @@ cursos <- read_sheet(hoja_calculo, sheet = "cursos-completos")
 
 # Totales
 
-n_paises <- datos %>%
+n_paises <- cursos %>%
   separate_rows(paises, sep = "/") %>%
   filter(!is.na(paises)) %>% 
   pull(paises) %>%
